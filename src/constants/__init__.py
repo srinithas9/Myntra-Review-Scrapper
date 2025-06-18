40a45866dd34caedd746dedde058337392888974
Srinitha .s <srinithas994@gmail.com>
2025-06-18 13:29:58 +0530

    final commit

---
+++ src/constants/__init__.py
@@ -0,0 +1,5 @@
+MONGODB_URL_KEY: str = "mongodb+srv://srinitha48:9845422927@cluster0.iqlx6qo.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0"
+MONGO_DATABASE_NAME: str = "myntra-reviews"
+
+
+SESSION_PRODUCT_KEY: str = "product_name"
